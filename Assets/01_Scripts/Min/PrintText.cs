dde1f8a82e356e3d35a46e57c0063ac6c8102eba
mingyo0125 <wjdalsry125@gmail.com>
2022-09-21 22:52:31 +0900

    0921 to heesoo textmanager

---
+++ Assets/01_Scripts/Min/PrintText.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintText : MonoBehaviour
+{
+    public ScriptableObject textDatas;
+    string text;
+
+
+    private void Awake()
+    {
+        //textDatas._list[].dataID;
+    }
+
+    public void _PrintText()
+    {
+        //switch (textDatas._list[].dataID)
+        //{
+        //    case "오노조":
+        //        text = textDatas.text;
+        //        FindObjectOfType<UIManager>().TextPrint(text);
+        //        break;
+        //    case "려차":
+        //        text = textDatas.text;
+        //        FindObjectOfType<UIManager>().TextPrint(text);
+        //        break;
+        //    case "안형주":
+        //        text = textDatas.text;
+        //        FindObjectOfType<UIManager>().TextPrint(text);
+        //        break;
+        //    case "황원준":
+        //        text = textDatas.text;
+        //        FindObjectOfType<UIManager>().TextPrint(text);
+        //        break;
+        //    case "김성호":
+        //        text = textDatas.text;
+        //        FindObjectOfType<UIManager>().TextPrint(text);
+        //        break;
+        //}
+    }
+}
--- Assets/01_Scripts/Min/UIManager.cs
+++ Assets/01_Scripts/Min/UIManager.cs
@@ -1,16 +1,28 @@
+using System.Net.Mime;
 using System.Security.Cryptography.X509Certificates;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
 
+    [SerializeField]
+    TextMeshProUGUI realText;
+    TextDatas textDatas;
     
     private void Awake()
     {
         Instance = this;
+        //textDatas = GetComponent<TextDatas>();
+        realText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void TextPrint(string text)
+    {
+        realText.text = text;
     }
 
 }
