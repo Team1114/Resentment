bccdc240d1722d6abd71a3e8c72193c42ab715a5
mingyo0125 <wjdalsry125@gmail.com>
2022-10-19 19:04:42 +0900

    finish?

--- Assets/01_Scripts/Min/UI/ViewOption.cs
+++ Assets/01_Scripts/Min/UI/ViewOption.cs
@@ -17,13 +17,15 @@ public class ViewOption : MonoBehaviour
     private void Start()
     {
         InitUI();
+        Screen.SetResolution(1920, 1080, true);
     }
 
     void InitUI()
     {
-        for(int i = 0; i < Screen.resolutions.Length; i++)
+        
+        for (int i = 0; i < Screen.resolutions.Length; i++)
         {
-            if(Screen.resolutions[i].refreshRate == 60)
+            if(Screen.resolutions[i].refreshRate == 59)
             {
                 resolutions.Add(Screen.resolutions[i]);
             }
@@ -35,26 +37,27 @@ public class ViewOption : MonoBehaviour
 
         int optionNum = 0;
 
-        foreach(Resolution item in resolutions)
+        foreach (Resolution item in resolutions)
         {
             TMP_Dropdown.OptionData optionData = new TMP_Dropdown.OptionData();
-            optionData.text = item.width + "X" + item.height + " " + item.refreshRate + "Hz";
+            optionData.text = item.width + "X" + item.height;
             resolutionDropDown.options.Add(optionData);
 
-            if(item.width == Screen.width && item.height == Screen.height)
+            if (item.width == Screen.width && item.height == Screen.height)
             {
                 resolutionDropDown.value = optionNum;
-                optionNum++;
+                optionNum++;               
             }
         }
         resolutionDropDown.RefreshShownValue();
 
         fullScreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
+
     }
 
     public void DropDownOptionChange(int a)
     {
-        resolutionNum = a;
+        resolutionNum = a;  
     }
 
     public void OKButtonClick()
@@ -64,6 +67,6 @@ public class ViewOption : MonoBehaviour
 
     public void ToggleBtn(bool isFull)
     {
-        screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+       screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
     }
 }
