61f211b9029c693358801610b9efa7fb3976c5c1
heesoo1114 <improveme7@gmail.com>
2022-11-24 14:03:22 +0900

    jump 수정

--- Assets/01_Scripts/Hee/Player/PlayerController.cs
+++ Assets/01_Scripts/Hee/Player/PlayerController.cs
@@ -66,18 +66,18 @@ public class PlayerController : MonoBehaviour
             if (hit.collider.CompareTag("Ground")) // 땅일 때
             {
                 isGround = true;
-                jumpPower = 8f; // 인스펙터에서 바꾸면 여기도 수정
+                jumpPower = 5f; // 인스펙터에서 바꾸면 여기도 수정
                 jumpCount = 2;
             }
             else if (!hit.collider.CompareTag("Ground")) // 공중일 때
             {
-                jumpPower = 4f; // 인스펙터에서 바꾸면 여기도 수정
+                jumpPower = 10f; // 인스펙터에서 바꾸면 여기도 수정
                 isGround = false;
             }
         }
         else // 공중일 때
         {
-            jumpPower = 4f; // 인스펙터에서 바꾸면 여기도 수정
+            jumpPower = 10f; // 인스펙터에서 바꾸면 여기도 수정
             isGround = false;
         }
 
